358466fc8b27f0769f1c73da56e077c42fb8ad82
RinesaSaraci <rinesasaraci1@gmail.com>
2026-04-27 17:10:17 +0200

    Database configuration

--- src/app.js
+++ src/app.js
@@ -1,4 +1,7 @@
+require("dotenv").config();
+
 const express = require("express");
+const sequelize = require("./config/db");
 const cors = require("cors");
 
 const app = express();
@@ -7,10 +10,28 @@ app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("Glamora API running... ");
+  res.send("Glamora API running...");
 });
 
-const PORT = 8080;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+const PORT = process.env.PORT || 8080;
+
+async function startServer() {
+  try {
+    
+    await sequelize.authenticate();
+    console.log("Database connected successfully!");
+
+ 
+    await sequelize.sync();
+
+   
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+
+  } catch (err) {
+    console.log("Database connection error:", err);
+  }
+}
+
+startServer();
